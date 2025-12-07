ddc0e4b1b1f2fb90d59b4446294a3e134c70153c
wonwoo je <wonwooje@wonwooui-iMac.local>
2025-12-07 16:19:19 +0900

    Auto-deploy from Admin Dashboard

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -3,9 +3,5 @@
 import ReactMarkdown from 'react-markdown';
 
 export default function MarkdownViewer({ content }: { content: string }) {
-    return (
-        <div className="prose prose-invert max-w-none p-8">
-            <ReactMarkdown>{content}</ReactMarkdown>
-        </div>
-    );
+    return <ReactMarkdown>{content}</ReactMarkdown>;
 }
